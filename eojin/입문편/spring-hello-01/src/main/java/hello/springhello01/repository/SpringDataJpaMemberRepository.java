be9d7b430943c55675c0d2ac079b1971801d4edb
Ginieee <kaj1226@naver.com>
2022-11-10 11:43:53 +0900

    [어진] : 입문편 실습

---
+++ eojin/입문편/spring-hello-01/src/main/java/hello/springhello01/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package hello.springhello01.repository;
+
+import hello.springhello01.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
---
+++ eojin/입문편/spring-hello-01/src/test/java/hello/springhello01/SpringHello01ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springhello01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHello01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
