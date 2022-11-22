7b4ef4f4dc2bec90f9feebc78cd6eced90fcbdb6
Ginieee <kaj1226@naver.com>
2022-11-23 00:25:24 +0900

    [어진] : Ch3 실습

--- eojin/기본편/core/src/main/java/hello/core/MemberApp.java
+++ eojin/기본편/core/src/main/java/hello/core/MemberApp.java
@@ -4,10 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+
+//        AppConfig appConfig = new AppConfig();
+//        //appConfig.memberService를 변수로 만드는 단축키 -> Ctrl+Alt+V
+//        MemberService memberService = appConfig.memberService(); //이 안에는 memberServiceImpl이 들어있는 것!
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
