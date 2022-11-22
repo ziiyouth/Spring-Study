15b925ebb343c771ec9cf11663698d591649d3ee
juwonleee <juwonlee0509@gmail.com>
2022-11-23 08:03:58 +0900

    [주원] :

--- juwon/core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ juwon/core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,10 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository=new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member){
@@ -11,6 +14,7 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public Member findMember(Long memberId) {
+
         return memberRepository.findById(memberId);
     }
 }
--- juwon/core/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ juwon/core/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,15 +1,24 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.*;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static hello.core.member.Grade.VIP;
 
 public class OrderServiceTest {
 
-    MemberService memberService=new MemberServiceImpl();
-    OrderService orderService=new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder(){
